9f5f0b2d5e4ef96cec6c3a91f4d7735bf3a1b9a0
Mahammad Eliyaz Shaik <8754342+basha008@users.noreply.github.com>
2026-07-26 21:03:29 +0530

    product-cart-aassignment 2nd commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductCart from '../../first_project/src/product_cart/product_cart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
